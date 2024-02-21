e65694a1d5c258131751836d61c689b7031c2bb7
Dennis Priskorn <egils@riseup.net>
2024-02-21 16:26:10 +0100

    feat: Implement basic query execution

--- qleverintegrator/__init__.py
+++ qleverintegrator/__init__.py
@@ -1,5 +1,22 @@
+import requests
 from pydantic import BaseModel
+from requests import Session
+
+
+class ActionError(BaseException):
+    pass
 
 
 class QleverIntegrator(BaseModel):
-    pass
+    endpoint: str = "https://qlever.cs.uni-freiburg.de/api/wikidata"
+    session: Session = requests.session()
+
+    def execute_query(self, query: str, action: str = "json_export"):
+        if action not in ["tsv_export", "json_export"]:
+            raise ActionError(f"Action {action} is not supported")
+        params = {
+            'query': query,
+            'action': 'json_export',
+        }
+
+        response = self.session.get('https://qlever.cs.uni-freiburg.de/api/wikidata', params=params)
